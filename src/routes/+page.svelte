b5a9e3c898f6e9dcec05c4803f1914e50fb86317
DepthPixels <depthclawclashedor@gmail.com>
2023-06-30 14:14:06 +0530

    Add Basic Babylon.js Scene
    Add a Basic Scene to work off of, the cube is rotatable and the view can be zoomed in and out. The Scene has one ArcRotateCamera, A Cube, and a HemisphericLight.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from 'svelte';
+  import { createScene } from "./scene";
+  let el;
+  onMount(() => {
+    createScene(el)
+  });
+</script>
+
+<canvas bind:this={el}></canvas>
+
+<style>
+  html, body {
+  margin: 0;
+  padding: 0;
+  }
+
+  canvas {
+    width: 100vw;
+    height: 100vh;
+    display: block;
+  }
+</style>
